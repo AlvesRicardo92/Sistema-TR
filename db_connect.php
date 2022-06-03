8f4199910f02bc809e625faa1bc565604d4dfb50
AlvesRicardo92 <ricardobalves3@gmail.com>
2022-06-03 16:20:11 -0300

    Alterado as credenciais de conexão

--- db_connect.php
+++ db_connect.php
@@ -1,8 +1,8 @@
 <?php
-define("HOST", "localhost"); // O host no qual você deseja se conectar.
-define("USER", "st_user"); // O nome de usuário do banco de dados.
-define("PASSWORD", "612915"); // A senha do usuário do banco de dados. 
-define("DATABASE", "secure_login"); // O nome do banco de dados.
+define("HOST", "10.1.0.230"); // O host no qual você deseja se conectar.
+define("USER", "r37484"); // O nome de usuário do banco de dados.
+define("PASSWORD", "r37484"); // A senha do usuário do banco de dados. 
+define("DATABASE", "inventario_informatica_se"); // O nome do banco de dados.
 
 $mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
 // Se você estiver se conectando via TCP/IP ao invés de um socket UNIX, lembre-se de adicionar o número da porta como um parâmetro.
